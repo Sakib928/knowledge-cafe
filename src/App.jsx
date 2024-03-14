10d2ad2b4eaf37a76f96784cb924bcc1170db1ee
Sakib928 <sakibgscian@gmail.com>
2024-03-14 10:36:29 +0600

    initial files created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold">Hola Amigos!</p>
+    </>
+  );
+}
+
+export default App;
